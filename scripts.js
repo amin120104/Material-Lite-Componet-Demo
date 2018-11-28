dc7097b7a840d44071e1584d09edee9646231dc3
amin04 <aminmithun1992@gmail.com>
2018-11-28 17:45:00 +0600

    new componet added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//show dialog
 var dialog = document.querySelector("dialog");
 var showModalButton = document.querySelector(".show-modal");
 if (!dialog.showModal) {
@@ -9,3 +10,25 @@ showModalButton.addEventListener("click", function() {
 dialog.querySelector(".close").addEventListener("click", function() {
   dialog.close();
 });
+
+//snackbar
+(function () {
+  'use strict';
+  var snackbarContainer = document.querySelector('#demo-snackbar-example');
+  var showSnackbarButton = document.querySelector('#demo-show-snackbar');
+  var handler = function (event) {
+    showSnackbarButton.style.backgroundColor = '';
+  };
+  showSnackbarButton.addEventListener('click', function () {
+    'use strict';
+    showSnackbarButton.style.backgroundColor = '#' +
+      Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    var data = {
+      message: 'Button color changed.',
+      timeout: 2000,
+      actionHandler: handler,
+      actionText: 'Undo'
+    };
+    snackbarContainer.MaterialSnackbar.showSnackbar(data);
+  });
+}());
